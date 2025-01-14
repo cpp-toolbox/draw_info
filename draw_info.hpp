52ad4cd92138007011325aa5c8b38af3fdbd2b9d
cuppajoeman <ccn@cuppajoeman.com>
2025-01-14 02:50:26 -0500

    feat: add packed ivps

--- draw_info.hpp
+++ draw_info.hpp
@@ -31,12 +31,13 @@ class IVPTextured {
   public:
     IVPTextured(std::vector<unsigned int> indices, std::vector<glm::vec3> xyz_positions,
                 std::vector<glm::vec2> texture_coordinates, const std::string &texture = "")
-        : indices(indices), xyz_positions(xyz_positions), texture_coordinates(texture_coordinates), texture(texture) {};
+        : indices(indices), xyz_positions(xyz_positions), texture_coordinates(texture_coordinates),
+          texture_path(texture) {};
     Transform transform;
     std::vector<unsigned int> indices;
     std::vector<glm::vec3> xyz_positions;
     std::vector<glm::vec2> texture_coordinates;
-    std::string texture;
+    std::string texture_path;
 };
 
 // with normals
@@ -46,13 +47,52 @@ class IVPNTextured {
                  std::vector<glm::vec3> normals, std::vector<glm::vec2> texture_coordinates,
                  const std::string &texture = "")
         : indices(indices), xyz_positions(xyz_positions), normals(normals), texture_coordinates(texture_coordinates),
-          texture(texture) {};
+          texture_path(texture) {};
     Transform transform;
     std::vector<unsigned int> indices;
     std::vector<glm::vec3> xyz_positions;
     std::vector<glm::vec3> normals;
     std::vector<glm::vec2> texture_coordinates;
-    std::string texture;
+    std::string texture_path;
+};
+
+class IVPTexturePacked {
+  public:
+    IVPTexturePacked(std::vector<unsigned int> indices, std::vector<glm::vec3> xyz_positions,
+                     std::vector<glm::vec2> packed_texture_coordinates, int packed_texture_index,
+                     int packed_texture_bounding_box_index, const std::string &texture)
+        : indices(indices), xyz_positions(xyz_positions), packed_texture_coordinates(packed_texture_coordinates),
+          packed_texture_index(packed_texture_index),
+          packed_texture_bounding_box_index(packed_texture_bounding_box_index), texture_path(texture) {};
+    Transform transform;
+    std::vector<unsigned int> indices;
+    std::vector<glm::vec3> xyz_positions;
+    std::vector<glm::vec2> packed_texture_coordinates;
+    int packed_texture_index;
+    int packed_texture_bounding_box_index;
+    std::string texture_path;
+};
+
+class IVPNTexturePacked {
+  public:
+    IVPNTexturePacked(std::vector<unsigned int> indices, std::vector<glm::vec3> xyz_positions,
+                      std::vector<glm::vec3> normals, std::vector<glm::vec2> original_texture_coordinates,
+                      std::vector<glm::vec2> packed_texture_coordinates, int packed_texture_index,
+                      int packed_texture_bounding_box_index, const std::string &texture)
+        : indices(indices), xyz_positions(xyz_positions), normals(normals),
+          original_texture_coordinates(original_texture_coordinates),
+          packed_texture_coordinates(packed_texture_coordinates), packed_texture_index(packed_texture_index),
+          packed_texture_bounding_box_index(packed_texture_bounding_box_index), texture_path(texture) {};
+    Transform transform;
+    int id = UniqueIDGenerator::generate();
+    std::vector<unsigned int> indices;
+    std::vector<glm::vec3> xyz_positions;
+    std::vector<glm::vec3> normals;
+    std::vector<glm::vec2> original_texture_coordinates;
+    std::vector<glm::vec2> packed_texture_coordinates;
+    int packed_texture_index;
+    int packed_texture_bounding_box_index;
+    std::string texture_path;
 };
 
 #endif // DRAW_INFO_HPP
